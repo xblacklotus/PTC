8b2daed6c3999ae71c498ac40e1148d872cdc0f9
Pana_Itr <maritho0p@gmail.com>
2014-09-22 17:55:01 -0600

    villegas diseño

--- index.php
+++ index.php
@@ -4,24 +4,18 @@
 	<div id="Aside_Log">
 		<h3>AULA VIRTUAL</h3>
 		<h5>SELECCIÓN DE TIPO DE USUARIO</h5>
-		<a href="pags/loginMaestro.php">
-		<div class="contenedor" id="uno">
-			<img class="icon" src="imgs/icon1.png" />
-			<p class="texto">Profesor</p>
-		</div>
-		</a>
-		<a href="pags/loginAlumno.php">
-		<div class="contenedor" id="dos">
-			<img class="icon" src="imgs/icon2.png" />
-			<p class="texto">Alumno</p>
-		</div>
-		</a>
-		<a href="pags/loginAdmin.php">
-		<div class="contenedor" id="tres">
-			<img class="icon" src="imgs/icon3.png" />
-			<p class="texto">Administrador</p>
-		</div>
-		</a>
+			<a href="pags/loginMaestro.php">
+			<div class="contenedor" id="uno">
+				<img class="icon" src="imgs/icon1.png" />
+				<p class="texto">Profesor</p>
+			</div>
+			</a>
+			<a href="pags/loginAdmin.php">
+			<div class="contenedor" id="tres">
+				<img class="icon" src="imgs/icon3.png" />
+				<p class="texto">Administrador</p>
+			</div>
+			</a>
 	</div>
 </div>
 
---
+++ index2.php
@@ -0,0 +1,16 @@
+<?php include("includes/header.php");?>
+
+<div id="Back">
+	<div id="Aside_Log">
+		<h3>AULA VIRTUAL</h3>
+		<h5>SELECCIÓN DE TIPO DE USUARIO</h5>
+		<a href="pags/loginAlumno.php">
+		<div class="contenedor" id="dos">
+			<img class="icon" src="imgs/icon2.png" />
+			<p class="texto">Alumno</p>
+		</div>
+		</a>
+	</div>
+</div>
+
+<?php include("includes/footer.php");?>    
