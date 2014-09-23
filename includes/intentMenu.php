2bb07f2026d63f41966065deca258e2e669330d3
Afroluis96 <luisaparicioj316@gmail.com>
2014-09-23 09:23:13 -0600

    menus
    diferentes estilos

---
+++ includes/intentMenu.php
@@ -0,0 +1,39 @@
+<?php include("config.inc");?>
+<!DOCTYPE html>
+<html><head>
+<title>PTC</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta name="description" content="" />
+<meta name="copyright" content="" />
+<link rel="stylesheet" type="text/css" href="../css/style2.css">
+<link rel="stylesheet" type="text/css" href="../css/kickstart.css" media="all" />                  <!-- KICKSTART -->
+<link rel="stylesheet" type="text/css" href="../style.css" media="all" /> 
+<link rel="stylesheet" type="text/css" href="../css/menu2.css" media="all" />                          <!-- CUSTOM STYLES -->
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script type="text/javascript" src="../js/kickstart.js"></script>                                  <!-- KICKSTART -->
+</head><body>
+
+
+<!-- Menu Horizontal -->
+<div >
+	<ul id="nav">
+	<li class="current"><a href="../pags/mantenimientos.php">Notas</a></li>
+	<li><a href="">Mantenimientos</a>
+			<ul>
+			<li><a href="../pags/mantenimiento_maestros.php">Maestros</a></li>
+			<li><a href="../pags/mantenimiento_materias.php">Materias</a></li>
+			<li><a href="../pags/mantenimiento_seccion.php">Sección</a></li>
+			<li><a href="../pags/mantenimiento_usuarios.php">Usuarios</a></li>
+
+			<li><a href="../pags/mantenimiento_alumnos.php">Alumnos</a></li>
+			<li><a href="../pags/mantenimiento_perfiles.php">Perfiles</a></li>
+			</ul>
+		</li>
+		<li><a href="../includes/desconectar_super_usuario.php">Cerrar sesión</a></li>
+		
+	</ul></div>
+
+<div class="grid">
+	
+<!-- ===================================== END HEADER ===================================== -->
