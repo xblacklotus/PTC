15627b2a4b82c1130181ecf1ccab741c3921a111
Afroluis96 <luisaparicioj316@gmail.com>
2014-09-23 19:38:31 -0600

    Menu conlinks

--- pags/Menu_Admin.php
+++ pags/Menu_Admin.php
@@ -14,12 +14,14 @@
             <h1>Mantenimiento </h1>
             <div class="content">
                 <ul class="bmenu">
-                    <li><a href="#">Alumnos</a></li>
-                    <li><a href="#">Maestros</a></li>
-                    <li><a href="#">Materias</a></li>
-                    <li><a href="#">Perfiles</a></li>
-                    <li><a href="#">Secciones</a></li>
-                    <li><a href="#">Administradores</a></li>
+                    <li><a href="mantenimiento_alumnos.php">Alumnos</a></li>
+                    <li><a href="mantenimiento_maestros.php">Maestros</a></li>
+                    <li><a href="mantenimiento_materias.php">Materias</a></li>
+                    <li><a href="mantenimiento_perfiles.php">Perfiles</a></li>
+                    <li><a href="mantenimiento_seccion.php">Secciones</a></li>
+                    <li><a href="mantenimiento_usuarios.php">Usuarios</a></li>
+                    <li><a href="mantenimiento_super">Administradores</a></li>
+                    <li><a href="mantenimientos.php">Nose como ponerle</a></li>
                 </ul>
             </div>
         </div>
