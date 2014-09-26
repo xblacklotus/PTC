2195e1058bb69d9e812487cb0f4bdbc5baad2349
Afroluis96 <luisaparicioj316@gmail.com>
2014-09-25 22:33:13 -0600

    medio

--- index.php
+++ index.php
@@ -1,22 +1,16 @@
 <?php include("includes/header.php");?>
 
-<div id="Back">
+<div id="Back2">
 	<div id="Aside_Log">
 		<h3>AULA VIRTUAL</h3>
 		<h5>SELECCIÓN DE TIPO DE USUARIO</h5>
-			<a href="pags/loginMaestro.php">
-			<div class="contenedor" id="uno">
-				<img class="icon" src="imgs/icon1.png" />
-				<p class="texto">Profesor</p>
-			</div>
-			</a>
-			<a href="pags/loginAdmin.php">
-			<div class="contenedor" id="tres">
-				<img class="icon" src="imgs/icon3.png" />
-				<p class="texto">Administrador</p>
-			</div>
-			</a>
+		<a href="pags/loginAlumno.php">
+		<div class="contenedor" id="dos">
+			<img class="icon" src="imgs/icon2.png" />
+			<p class="texto">Alumno</p>
+		</div>
+		</a>
 	</div>
 </div>
 
-<?php include("includes/footer2.php");?>             
+<?php include("includes/footer2.php");?>    
---
+++ index_supremo.php
@@ -0,0 +1,22 @@
+<?php include("includes/header.php");?>
+
+<div id="Back">
+	<div id="Aside_Log">
+		<h3>AULA VIRTUAL</h3>
+		<h5>SELECCIÓN DE TIPO DE USUARIO</h5>
+			<a href="pags/loginMaestro.php">
+			<div class="contenedor" id="uno">
+				<img class="icon" src="imgs/icon1.png" />
+				<p class="texto">Profesor</p>
+			</div>
+			</a>
+			<a href="pags/loginAdmin.php">
+			<div class="contenedor" id="tres">
+				<img class="icon" src="imgs/icon3.png" />
+				<p class="texto">Administrador</p>
+			</div>
+			</a>
+	</div>
+</div>
+
+<?php include("includes/footer2.php");?>             
